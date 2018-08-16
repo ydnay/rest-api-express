2da3fb9e05f060193a58bf69139ea9d04b18eaa2
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-08-16 08:29:07 -0400

    Add routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+// const cors         = require('cors');
 
 mongoose.Promise = Promise;
 mongoose
@@ -24,6 +25,7 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 const app = express();
 
 // Middleware Setup
+// app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -45,9 +47,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+// Routes
 const index        = require('./routes/index');
 const phonesApi    = require('./routes/phones-api');
 app.use('/', index);
 app.use('/api', phonesApi);
+
+// This will be the default route is nothing else is caught
+// app.use(function(req, res) {
+//   res.sendfile(__dirname + '/public/index.html');
+// });
 
 module.exports = app;
--- routes/phones-api.js
+++ routes/phones-api.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express   = require('express');
+const router    = express.Router();
+const mongoose  = require('mongoose');
 
 const Phone = require('../models/phone-model');
 //import { Phone } from '../models/phone-model';
 
 /* GET Phones listing. */
 router.get('/phones', (req, res, next) => {
-  Phone.find(phonesList)
-  .then(phonesList => {
+  Phone.find()
+  .then((phonesList, err) => {
     if (err) {
       res.json(err);
       return;
@@ -17,8 +18,8 @@ router.get('/phones', (req, res, next) => {
   .catch(error => next(error))
 });
 
-/* CREATE a new Phone. */
-router.post('/phones', (req, res, next) => {
+ /* CREATE a new Phone. */
+ router.post('/phones', (req, res, next) => {
   const thePhone = new Phone({
     brand: req.body.brand,
     name: req.body.name,
@@ -31,6 +32,59 @@ router.post('/phones', (req, res, next) => {
     res.json({
       message: 'New Phone created!',
       id: thePhone._id
+    });
+  })
+  .catch(error => next(error))
+});
+
+/* GET a single Phone. */
+router.get('/phones/:id', (req, res, next) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Phone.findById(req.params.id)
+  .then(thePhone => {
+      res.json(thePhone);
+  })
+  .catch(error => next(error))
+});
+
+/* EDIT a Phone. */
+router.put('/phones/:id', (req, res, next) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  const updates = {
+    brand: req.body.brand,
+    name: req.body.name,
+    specs: req.body.specs,
+    image: req.body.image
+  };
+
+  Phone.findByIdAndUpdate(req.params.id, updates)
+  .then(phone => {
+    res.json({
+      message: 'Phone updated successfully'
+    });
+  }) 
+  .catch(error => next(error))     
+})
+
+/* DELETE a Phone. */
+router.delete('/phones/:id', (req, res, next) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Phone.remove({ _id: req.params.id })
+  .then(message => {
+    return res.json({
+      message: 'Phone has been removed!'
     });
   })
   .catch(error => next(error))
